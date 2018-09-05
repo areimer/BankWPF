fce58ac96fbdab23e33849f062352177035c5b8c
Assman <LukasArndt94@yahoo.de>
2018-09-05 14:52:01 +0200

    mainview

---
+++ AWEBank/BankWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BankWPF.Views;
+using System.Windows.Input;
+using BankWPF.Commands;
+using System.Drawing;
+
+
+namespace BankWPF.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        /* MainWindow Properties */
+        private UIElement _content = new MainView();
+        public string Path { get; private set; }
+        public ICommand ClickTeamsCommand { get; set; }
+        public ICommand ClickGroupCommand { get; set; }
+        public ICommand ClickPlanCommand { get; set; }
+        public ICommand ClickTipCommand { get; set; }
+        public ICommand BackToMain { get; set; }
+
+        /* Content Getter und Setter */
+        public UIElement Content
+        {
+            get { return _content; }
+            set { _content = value; OnPropertyChanged(nameof(Content)); }
+        }
+
+        /* MainWindow Konstruktor */
+        public MainWindowViewModel()
+        {      
+            /*
+            ClickTeamsCommand = new ActionCommand(loadTeamView, _ => Content is MainMenu);
+            ClickGroupCommand = new ActionCommand(loadGroupView, _ => Content is MainMenu);
+            ClickPlanCommand = new ActionCommand(loadPlanView, _ => Content is MainMenu);
+            ClickTipCommand = new ActionCommand(loadTipView, _ => Content is MainMenu);
+            BackToMain = new ActionCommand(_ => Content = new MainMenu(), _ => !(Content is MainMenu));
+            */
+            Path = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName + "\\Resources\\Background.png";
+        }
+
+        /* Lädt die Teamübersicht in den Content 
+        public void loadTeamView(Object param)
+        {
+            Content = new TeamView();
+        }
+        /* Lädt die Gruppenansicht in den Content 
+        public void loadGroupView(Object param)
+        {
+            Content = new GroupView();
+        }
+        /* Lädt den Spielplan in den Content 
+        public void loadPlanView(Object param)
+        {
+            Content = new PlanView();
+        }
+        /* Lädt den Tipper in den Content 
+        public void loadTipView(Object param)
+        {
+            Content = new TipView();
+        }
+        */
+    }
+}
