4d37498186fc9c3faf910dcfe31e994fa5aba96c
unknown <User@K05-1004-P028.rbz-technik-kiel.de>
2018-09-12 13:03:12 +0200

    collections angelegt

---
+++ AWEBank/BankObj/KundeCol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankObj;
+
+namespace BankObj
+{
+    public class KundeCol : ObservableCollection<Kunde> { }
+}
